8de6b6360e10f89c0c2aef94d410a24fe0c1e0f5
lntanon55 <148826779+lntanon55@users.noreply.github.com>
2023-10-28 11:38:02 +0700

    DarkraiUi.lua

--- DarkraiUi.lua
+++ DarkraiUi.lua
@@ -1,23 +1,103 @@
-local DarkraiX = loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroscgaming/zeroscgaming/main/RDE.lib/Dark%20UI", true))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Glof Hub l Blade ball", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Library = DarkraiX:Window("Golf HUB","Blox fruit","RightAlt",Enum.KeyCode.RightControl);
-local GG = Library:AddTab("Main","")
-GG:AddSeperator("กดติดตามด้วยนะ")
+OrionLib:MakeNotification({
+	Name = "Title!",
+	Content = "Notification content... what will it say??",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
 
-Time = GG:AddLabel("ฟังก์ชั่น ฟาร์ม")
-function UpdateTime()
-local GameTime = math.floor(workspace.DistributedGameTime+0.5)
-local Hour = math.floor(GameTime/(60^2))%24
-local Minute = math.floor(GameTime/(60^1))%60
-local Second = math.floor(GameTime/(60^0))%60
-Time:Set("Hr(s) : "..Hour.." Min(s) : "..Minute.." Sec(s) : "..Second)
+
+
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "farm"
+})
+
+Tab:AddToggle({
+	Name = "FastAttack",
+	Default = false,
+	Callback = function(Value)
+(getgenv()).Config = {
+ ["FastAttack"] = Value,
+ ["ClickAttack"] = Value
+} 
+
+coroutine.wrap(function()
+local StopCamera = require(game.ReplicatedStorage.Util.CameraShaker)StopCamera:Stop()
+    for v,v in pairs(getreg()) do
+        if typeof(v) == "function" and getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework then
+             for v,v in pairs(debug.getupvalues(v)) do
+                if typeof(v) == "table" then
+                    spawn(function()
+                        game:GetService("RunService").RenderStepped:Connect(function()
+                            if getgenv().Config['FastAttack'] then
+                                 pcall(function()
+                                     v.activeController.timeToNextAttack = -(math.huge^math.huge^math.huge)
+                                     v.activeController.attacking = false
+                                     v.activeController.increment = 4
+                                     v.activeController.blocking = false   
+                                     v.activeController.hitboxMagnitude = 150
+    		                         v.activeController.humanoid.AutoRotate = true
+    	                      	     v.activeController.focusStart = 0
+    	                      	     v.activeController.currentAttackTrack = 0
+                                     sethiddenproperty(game:GetService("Players").LocalPlayer, "SimulationRaxNerous", math.huge)
+                                 end)
+                             end
+                         end)
+                    end)
+                end
+            end
+        end
+    end
+end)();
+
+spawn(function()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if getgenv().Config['ClickAttack'] then
+             pcall(function()
+                game:GetService'VirtualUser':CaptureController()
+			    game:GetService'VirtualUser':Button1Down(Vector2.new(0,1,0,1))
+            end)
+        end
+    end)
+end)
+	end})
+	
+Tab:AddToggle({
+	Name = "bringmob",
+	Default = false,
+	Callback = function(Value)
+_G.bringmob = Value
+while _G.bringmob do wait()
+    pcall(function()
+for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+for x,y in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+if v.Name == "Bandit [Lv. 5]" then
+    if y.Name == "Bandit [Lv. 5]" then
+   v.HumanoidRootPart.CFrame = y.HumanoidRootPart.CFrame
+   v.HumanoidRootPart.Size = Vector3.new(60,60,60)
+   y.HumanoidRootPart.Size = Vector3.new(60,60,60)
+   v.HumanoidRootPart.Transparency = 1
+   v.HumanoidRootPart.CanCollide = false
+   y.HumanoidRootPart.CanCollide = false
+   v.Humanoid.WalkSpeed = 0
+   y.Humanoid.WalkSpeed = 0
+   v.Humanoid.JumpPower = 0
+   y.Humanoid.JumpPower = 0
+   if sethiddenproperty then
+     sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
 end
-
-GG:AddButton("Button",function()
+end
+end
+end
+end
 end)
-GG:AddToggle("ออโต้ฟาร์ม",function()
-end)
-GG:AddDropdown("เลือกอาวุธ", {"ดาบ","หมัด","ผลไม้ปีศาจ"},function(GG)
-end)
-GG:AddSlider("GAY At ",100200,300,function(GA)
-end)
+end
+	end}
